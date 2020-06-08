7134ec24b3a9d228c3fd68db2259366ed0393bef
Ruslan Zherebytskyi <rusikmvp@gmail.com>
2020-06-08 20:47:12 +0300

    Update axios-quiz.js

--- src/axios/axios-quiz.js
+++ src/axios/axios-quiz.js
@@ -3,17 +3,3 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://react-quiz-5fa98.firebaseio.com/',
 })
-
-const money = 1000;
-const price = 12000;
-const loanMaxMonth = 10;
-
-
-const monthlyPayment= () => {
-  return price / money 
-}
-
-const can
-
-
-console.log(monthlyPayment)
